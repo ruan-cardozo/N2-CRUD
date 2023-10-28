e8c5ce3b70ba25a101a1b6d4766f38b2f46e5497
ruan-cardozo <ruanvcardozo@gmail.com>
2023-10-28 16:05:49 -0300

    fix: arrumar identação do código

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,24 @@
-const mysql2 = require('mysql2/promise');
-const util = require('util');
+const mysql2 = require("mysql2/promise");
+const util = require("util");
 
 async function connect() {
-    if (global.connection && global.connection.state !== 'disconnected') {
-        return global.connection;
-    }
+  if (global.connection && global.connection.state !== "disconnected") {
+    return global.connection;
+  }
 
-    try {
-        const connection = await mysql2.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: '',
-            database: 'bicicleta',
-        });
-        console.log("Conectou no MySQL!");
-        global.connection = connection;
-        return connection;
-    } catch (e) {
-        console.log('Ocorreu um erro ao conectar no MySQL!', e);
-    }
+  try {
+    const connection = await mysql2.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "",
+      database: "bicicleta",
+    });
+    console.log("Conectou no MySQL!");
+    global.connection = connection;
+    return connection;
+  } catch (e) {
+    console.log("Ocorreu um erro ao conectar no MySQL!", e);
+  }
 }
 
 module.exports = { connect };
--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const { router } = require("./routes/bicicleta_routes.js");
 let server = express();
 
 server.use(express.json());
-server.use(express.urlencoded({extended : true}));
-server.use("/",router);
+server.use(express.urlencoded({ extended: true }));
+server.use("/", router);
 
-server.listen(3000, function (){
-    console.log("port 3000");
-})
+server.listen(3000, function () {
+  console.log("port 3000");
+});
 
 module.exports = { server };
