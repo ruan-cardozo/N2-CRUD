12ec63c0921e6696dc84484d363968bf4045fe42
ruan-cardozo <ruanvcardozo@gmail.com>
2023-10-28 15:53:22 -0300

    add: con dentro de funcoes async e adicionar logs

--- controller/bicicleta_controller.js
+++ controller/bicicleta_controller.js
@@ -2,28 +2,31 @@ const { connect } = require("../config/connection.js");
 
 let bicicleta = {};
 
-const con = await connect();
 
 
 bicicleta.all = async function (req, res) {
+  const con = await connect();
   try {
     let bicicletas = await con.query("SELECT * FROM bicicleta;");
     res.send(bicicletas);
+    console.log("Busca efetuada com sucesso");
   } catch (e) {
     console.log("Ocorreu um erro ao buscar as bicicletas!", e);
   }
 };
 
 bicicleta.create = async function (req, res) {
+    const con = await connect();
     try {
         let bicicleta = req.body;
-        let sql = "INSERT INTO bicicleta(codigo_bicicleta, marca_bicicleta,cor_bicicleta) VALUES (?,?,?,);";
+        let sql = "INSERT INTO bicicleta(codigo_bicicleta, marca_bicicleta, cor_bicicleta) VALUES (?,?,?);";
         let values = [bicicleta.codigo_bicicleta, bicicleta.marca_bicicleta, bicicleta.cor_bicicleta];
-            let result = await con.query(sql, [values]);
+            let result = await con.query(sql, values);
         res.send({
             status: "Inserção efetuada com sucesso",
             result: result
         });
+        console.log("Inserção efetuada com sucesso");
     } catch (e) {
         console.log("Ocorreu um erro ao inserir o registro", e);
     }
