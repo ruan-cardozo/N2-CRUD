ed09515c84f4792a8d0e634aae5d69e52d936a3c
ruan-cardozo <ruanvcardozo@gmail.com>
2023-10-28 16:05:11 -0300

    add: adicionar rota para edição ded uma bicicleta

--- routes/bicicleta_routes.js
+++ routes/bicicleta_routes.js
@@ -3,7 +3,8 @@ const { bicicleta } = require("../controller/bicicleta_controller.js");
 
 let router = express.Router();
 
-router.get("/bicicletas", bicicleta.all);
+router.get("/bicicletas", bicicleta.read);
 router.post("/bicicletas/inserir", bicicleta.create);
+router.put("/bicicletas/alterar/:codigo_bicicleta", bicicleta.update);
 
 module.exports = { router };
