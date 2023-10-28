58ca20df5312878aff95e916e5f1297e3bbd6717
ruan-cardozo <ruanvcardozo@gmail.com>
2023-10-28 15:53:40 -0300

    add: rota para inserir dados novos de bicicletas

--- routes/bicicleta_routes.js
+++ routes/bicicleta_routes.js
@@ -5,5 +5,5 @@ let router = express.Router();
 
 router.get("/bicicletas", bicicleta.all);
 router.post("/bicicletas/inserir", bicicleta.create);
-router.put("/bicicletas/alterar/:id", bicicleta.update);
+
 module.exports = { router };
