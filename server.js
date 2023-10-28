128f2b14215443da6e7c653b78891e10e7e5cffa
ruan-cardozo <ruanvcardozo@gmail.com>
2023-10-28 15:07:05 -0300

    configurar servidor e conexao com o banco de dados

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const { router } = require("./routes/bicicleta_routes.js");
+
+let server = express();
+
+server.use(express.json());
+server.use(express.urlencoded({extended : true}));
+server.use("/",router);
+
+server.listen(3000, function (){
+    console.log("port 3000");
+})
+
+exports.server = server;
