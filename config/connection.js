bb0b88e5dd13b7f148f10dc57305cf2c09e7d6ac
ruan-cardozo <ruanvcardozo@gmail.com>
2023-10-28 21:56:07 -0300

    add: aplicando as variaveis ao connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,11 @@
 const mysql2 = require("mysql2/promise");
 const util = require("util");
+require('dotenv').config();
+
+const dbHost = process.env.DB_HOST;
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASSWORD;
+const dbname = process.env.DB_NAME; 
 
 async function connect() {
   if (global.connection && global.connection.state !== "disconnected") {
@@ -7,11 +13,11 @@ async function connect() {
   }
 
   try {
-    const connection = await mysql2.createConnection({
-      host: "localhost",
-      user: "root",
-      password: "",
-      database: "bicicleta",
+      const connection = await mysql2.createConnection({
+      host: dbHost,
+      user: dbUser,
+      password: dbPass ,
+      database: dbname,
     });
     console.log("Conectou no MySQL!");
     global.connection = connection;
